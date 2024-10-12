a9ae4430d972f3e1c8a65328a26d54a4ea0c0fd5
Cmdr-Raygun <renttibwerdna@gmail.com>
2024-10-12 00:20:33 -0500

    W6 Files

--- program_1.py
+++ program_1.py
@@ -1,11 +1,36 @@
+# Author: Andrew Bittner
+# Date: 10/10/2024
 
 # Program #1: Random Dice
 # Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
 # The dice sum will be the output of this function.
+import random
 
-def randDice():
+def exitSequence():
+    input('\nPress any key to exit... ')
+
+def randDice(dice_sides, dice_num):
     # Write your logic to generate 2 numbers between 1 and 6 here
+    dice_out = 0
+    for rolls_count in range(dice_num):
+        dice_out += random.randint(1, dice_sides)
+    return dice_out
 
+def getDiceAvg(rolls, dice_sides, dice_num):
+    dice_avg = 0
+    for rolls_count in range(rolls):
+        dice_avg += randDice(dice_sides, dice_num)
+    dice_avg /= rolls
+    return dice_avg
+
+def main():
+    rolls = 100
+    dice_sides = 6
+    dice_num = 2
+    dice_avg = getDiceAvg(rolls, dice_sides, dice_num)
+    print(f'The average result of {rolls} roll(s) of {dice_num} {dice_sides}-sided die/dice is {dice_avg}', end = '. \n')
+    exitSequence()
+main()
     # Sum 2 numbers
 
     # return sum to calling function
